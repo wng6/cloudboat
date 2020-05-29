0f8a7e364dae728d9f68d55079d403090dc27def
wng6 <dannyslee@me.com>
2020-05-29 13:19:44 +0800

    add project-2

---
+++ src/main/java/com/woniuxy/springcloud/CloudBoatApplication.java
@@ -0,0 +1,8 @@
+package com.woniuxy.springcloud;
+
+/**
+ * @author lizhiwen
+ * @create 2020-05-29 1:18 下午
+ */
+public class CloudBoatApplication {
+}
---
+++ src/test/java/com/woniuxy/springcloud/Test.java
@@ -0,0 +1,8 @@
+package com.woniuxy.springcloud;
+
+/**
+ * @author lizhiwen
+ * @create 2020-05-29 1:19 下午
+ */
+public class Test {
+}
